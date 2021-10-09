66fe53bfa85cfd76e8a6fa9ccee055d1499d2c58
Roger Oporta <roportaperez@gmail.com>
2021-10-09 10:59:56 -0600

    babel import

--- webpack.prod.js
+++ webpack.prod.js
@@ -56,7 +56,17 @@ module.exports = {
                         }
                     }
                 ]
-            }
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
         ]
     },
     plugins: [
